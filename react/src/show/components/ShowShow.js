946054cfd71ed72e0355c61e6f6162230908042e
egiblin <egiblin88@gmail.com>
2017-02-13 12:05:44 -0500

    show page styling

--- react/src/show/components/ShowShow.js
+++ react/src/show/components/ShowShow.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router';
 
 const ShowShow = ({ id, name, location, image_url, slots, date, time, duration, creator, price }) => {
     return(
-      <div className="show">
-        <h1> {name}</h1>
-        <h3>Location: {location} </h3>
-        <img src={image_url} />
+      <div className="show-container">
+        <div className="show">
+          <img src={image_url} />
+        </div>
+        <div className="show-info">
+          <h1> {name}</h1>
+          <h3>Location: {location} </h3>
+          <h3>Open Slots: {slots} </h3>
+        </div>
       </div>
     );
   };
