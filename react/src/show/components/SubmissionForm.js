083ee64c25eaa8b431719dce12367e4b5b14e144
egiblin <egiblin88@gmail.com>
2017-02-27 21:12:10 -0500

    submissions

--- react/src/show/components/SubmissionForm.js
+++ react/src/show/components/SubmissionForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SubmissionForm = props => {
   let team_options = props.teams.map(team => {
     return (
-      <option value={team.name}>With: {team.name}</option>
+      <option value={team.name}>{team.name}</option>
     );
   });
   if (props.clicked){
