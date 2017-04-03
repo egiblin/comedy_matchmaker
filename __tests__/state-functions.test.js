e73993e93412399c7b7a741ff21b04fcc19a59f5
egiblin <egiblin88@gmail.com>
2017-04-02 22:50:18 -0400

    jest test setup

--- __tests__/state-functions.test.js
+++ __tests__/state-functions.test.js
@@ -1,5 +1,5 @@
 describe('Addition', () => {
-  it('knows that 2 and 5 make 7', () => {
-    expect(2 + 5).toBe(7);
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
   });
 });
